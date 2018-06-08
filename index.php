bf29641585f4fa9ab5c8cbfa25380c65490a35bf
azmarifuzzaman <azm.arifuzzaman@gmail.com>
2018-06-09 08:25:54 +1000

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
     <body>
 <div align="center">
-                <h1>Hello Cloud Guru's</h1>
+                <h1>Hello Cloud Guru's; This is Arif's version of index.php file from Ryan's github for OpsWorks practice</h1>
                 <h2>Congratulations! You have successfully installed an application to OpsWorks!</h2>
                 <div align="center"><img src="https://s3.amazonaws.com/acloudguru-opsworkslab-donotdelete/logo-on-black-800.png"></div>
                 <p>You ACloudGuru Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
